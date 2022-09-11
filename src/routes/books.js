054ce84741badada5eac2e7400b50585722dee63
Sheriff Adekunle Tajudeen <dnexteinstein2@gmail.com>
2022-09-11 09:48:06 +0100

    Feature - Get all users: Add authentication/authorization for /users/feature_get_all Implement logic of /users/get_all routes

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,36 +1,36 @@
 const express = require('express');
-const bookRoutes = express.Router();
+const booksRoute = express.Router();
 
 
-bookRoutes.get('/', (req, res) => {
+booksRoute.get('/', (req, res) => {
     res.send('Books Page');
 });
 
 
-bookRoutes.post('/create', (req, res) => {
+booksRoute.post('/create', (req, res) => {
     res.send('Add new book');
 });
 
 
-bookRoutes.delete('/delete', (req, res) => {
+booksRoute.delete('/delete', (req, res) => {
     res.send('Delete a book');
 });
 
 
-bookRoutes.post('/loan_out', (req, res) => {
+booksRoute.post('/loan_out', (req, res) => {
     res.send('Loan out book');
 });
 
 
-bookRoutes.post('/return', (req, res) => {
+booksRoute.post('/return', (req, res) => {
     res.send('Return book');
 });
 
-bookRoutes.put('/update', (req, res) => {
+booksRoute.put('/update', (req, res) => {
     res.send('Update book');
 });
 
 
 
 
-module.exports = bookRoutes;
+module.exports = booksRoute;
--- test/integration/users.test.js
+++ test/integration/users.test.js
@@ -1,53 +1,73 @@
 const app = require('../../src/app');
-const { getPath } = require('../../src/utilities');
+const { getPath, returnAllRecords } = require('../../src/utilities');
 const supertest = require('supertest');
+const res = require('express/lib/response');
+
+const usersDbPath = getPath(__dirname, ['integration'], ['fixutures', 'stubs']);
 
 
 
 
 describe('Route: /users/create, Method: POST', () => {
     it('Confirms that a valid input is sent in the request', async () => {
-        const requestBody = {
+        const userDetails = {
             "username": "testMeji",
             "password": "test1"
         }
 
-        const response = await supertest(app).post('/users/create').send(requestBody);
+        const response = await supertest(app).post('/users/create').send(userDetails);
         expect(response.statusCode).toBe(201);
         expect(response.text).toBe(JSON.stringify({ message: "Thanks for registering. Your details have been saved." }));
     });
 
     it ('Checks if a username is provided', async () => {
-        const requestBody = {
-            "password": "test1"
+        const userDetails = {
+            "password": "badofor10"
         }
 
-        const response = await supertest(app).post('/users/create').send(requestBody);
+        const response = await supertest(app).post('/users/create').send(userDetails);
         expect(response.statusCode).toBe(400);
         expect(response.text).toBe(JSON.stringify({ message: "Please, enter a username." }));
     });
 
     it ('Checks if a password is provided', async () => {
-        const requestBody = {
-            "password": "test1"
+        const userDetails = {
+            "username": "badoforlife"
         }
-        const response = await supertest(app).post('/users/create').send(requestBody);
+        const response = await supertest(app).post('/users/create').send(userDetails);
         expect(response.statusCode).toBe(400);
         expect(response.text).toBe(JSON.stringify({ message: "Please, enter a password." }));
     });
 
     it ('Checks if username provided is not already used', async () => {
-        const requestBody = {
-            "username": "testMeji",
-            "password": "test1"
+        const userDetails = {
+            "username": "adekunle100",
+            "password": "adekunlepass",
         }
         
         //Determine if username already exist
 
 
 
-        const response = await supertest(app).post('/users/create').send(requestBody);
+        const response = await supertest(app).post('/users/create').send(userDetails);
         expect(response.statusCode).toBe(400);
         expect(response.text).toBe(JSON.stringify({ message: "Sorry, this username already exists." }));
     });
+});
+
+
+
+
+describe('Route: /users/get_all, Method: GET', () => {
+    it('Returns all users if request is by an admin', async() => {
+        const users = await returnAllRecords(usersDbPath);
+        const loginDetials = {
+            "username": "adekunle100",
+            "password": "adekunlepass",
+        }
+        const response = await supertest(app).get('/users/get_all').send(loginDetials);
+        expect(res.statusCode).toBe(200);
+        expect(res.text).toBe(users);
+    });
+
 });
